40d71c3920a768f77381d7440cae044cdc7b2a68
Erick Koga <pessoal@erickkoga.com>
2023-08-06 22:12:15 -0300

    feat(page): add home page introducing the developer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,35 @@
-export default function Home() {
+import Button from "@/components/Button";
+import Navbar from "@/components/Navbar";
+
+const Home = () => {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <p>Hello world</p>
-    </main>
+    <div className="flex">
+      <Navbar />
+      <main className="grow bg-gradient-to-r from-slate-50 to-white">
+        <section className="flex flex-col items-center justify-center h-screen gap-4">
+          <h1 className="text-6xl">
+            Meu nome é{" "}
+            <span
+              style={{
+                textShadow: "1px 1px 4px #000",
+              }}
+            >
+              Erick Koga
+            </span>
+          </h1>
+          <h2 className="text-4xl">Sou um desenvolvedor full-stack</h2>
+          <div className="flex gap-4">
+            <Button intent="secondary" size="lg">
+              Meu currículo
+            </Button>
+            <Button intent="primary" size="lg">
+              Meus projetos
+            </Button>
+          </div>
+        </section>
+      </main>
+    </div>
   );
-}
+};
+
+export default Home;
